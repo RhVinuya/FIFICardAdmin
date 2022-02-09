d8ca3a74cd5f091112f1bf1e96a0bddf8886e039
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-02-09 22:42:33 +0800

    Fix hosting config Fix loading of Cards list

--- src/app/cards/card-list/card-list.component.ts
+++ src/app/cards/card-list/card-list.component.ts
@@ -41,6 +41,9 @@ export class CardListComponent implements OnInit {
         this.withRecords = false;
       }
       this.initalizing = false;
+    }).catch(reason =>{
+      this.withRecords = false;
+      this.initalizing = false;
     });
   }
   
