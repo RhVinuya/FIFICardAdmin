5e5e7152dc011aa58413ec28daa041e634055f5d
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-02-26 23:41:15 +0800

    Update Order page filtering

--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -2,6 +2,7 @@ import { OrdersService } from './../../services/orders.service';
 import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/models/order';
 import { MatTableDataSource, PageEvent, Sort } from '@angular/material';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-order-list',
@@ -10,6 +11,9 @@ import { MatTableDataSource, PageEvent, Sort } from '@angular/material';
 })
 export class OrderListComponent implements OnInit {
   service: OrdersService;
+
+  fb: FormBuilder;
+  searchForm: FormGroup;
 
   orders: Order[];
   displayOrder: Order[] = [];
@@ -21,18 +25,27 @@ export class OrderListComponent implements OnInit {
   initializing: boolean = false;
   withRecords: boolean = false;
   status: string[] = [];
+  cards: string[] = [];
   filterStatus: string = '';
   dataSource: MatTableDataSource<Order  > = new MatTableDataSource();
   displayedColumns: string[] = ['card', 'sender', 'recipient', 'anonymously', 'sendto', 'address', 'status', 'date', 'action'];
 
   constructor(
-    _service: OrdersService
+    private _service: OrdersService,
+    private _fb: FormBuilder
   ) { 
     this.service = _service;
+    this.fb = _fb;
   }
 
   ngOnInit() {
     this.loadOrders();
+
+    this.searchForm = this.fb.group({
+      status: ['All'],
+      card: ['All'],
+      search: ['']
+    })
   }
 
   loadOrders(){
@@ -41,7 +54,7 @@ export class OrderListComponent implements OnInit {
 
     this.service.getOrders().then(orders => {
       this.orders = orders;
-      this.filterRecords(this.orders);
+      this.searchRecords('', 'All', 'All');
       this.generateFilter();
       
       this.initializing = false;
@@ -57,9 +70,9 @@ export class OrderListComponent implements OnInit {
     this.status.slice();
     if (this.orders){
       if(this.orders.length > 0){
-        this.status.push('All');
         this.orders.forEach(order => {
           this.addStatus(order.status.trim());
+          this.addCard(order.card_name.trim());
         })
       }
     }
@@ -76,39 +89,18 @@ export class OrderListComponent implements OnInit {
       this.status.push(_status);
   }
 
-  onStatusFilterChange(_status){
-    let value: string = _status.value;
-    this.filterStatus = value;
-    this.filterRecords(this.orders);
+  addCard(_card: string)
+  {
+    let isFound: Boolean = false;
+    this.cards.forEach(card => {
+      if(_card == card){
+        isFound = true;
+      }
+    });
+    if (!isFound)
+      this.cards.push(_card);
   }
-
-  filterRecords(_orders: Order[]){
-    if ((this.filterStatus == '') || (this.filterStatus == 'All')){
-      this.length = this.orders.length;
-      this.displayOrder = _orders;
-      this.updateRange();
-      //this.dataSource.data = this.orders;
-    }
-    else{
-      let data: Order[] = [];
-      _orders.forEach(order => {
-        if (order.status.trim() == this.filterStatus){
-          data.push(order);
-        }
-      });
-      this.length = data.length;
-      this.displayOrder = data;
-      this.updateRange();
-      //this.dataSource.data = data;
-    }
-  }
-
-  onPageChange(event: PageEvent){
-    this.pageSize = event.pageSize;
-    this.pageIndex = event.pageIndex;
-    this.updateRange();
-  }
-
+  
   updateRange(){
     let start: number = this.pageIndex * this.pageSize;
     let end: number = start + this.pageSize;
@@ -119,5 +111,71 @@ export class OrderListComponent implements OnInit {
       }
     }
     this.dataSource.data = selectedOrders;
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.updateRange();
+  }
+
+  searchCard(){
+    let search: string = this.searchForm.value['search'];
+    let card: string = this.searchForm.value['card'];
+    let status: string = this.searchForm.value['status'];
+    this.searchRecords(search, card, status);
+  }
+
+  searchRecords(search: string, card: string, status: string){
+    this.initializing = true;
+    this.withRecords = true ;
+    this.displayOrder = [];
+
+    if ((search.length == 0) && (status == 'All') && (card == 'All')){
+      this.displayOrder = this.orders;
+      this.length = this.displayOrder.length;
+      this.updateRange();
+    }
+    else{
+      this.orders.forEach(order => {
+        let searchMatch: boolean = true;
+        let cardMatch: boolean = true;
+        let statusMatch: boolean = true;
+
+        if (search.length > 0){
+          if (order.sender_name.includes(search)) {
+            searchMatch = true;
+          }
+          else if (order.receiver_name.includes(search)) {
+            searchMatch = true;
+          }
+          else{
+            searchMatch = false;
+          }
+        }
+
+        if (card != 'All'){
+          if (order.card_name.trim() != card.trim()){
+            cardMatch = false;
+          }
+        }
+
+        if (status != 'All'){
+          if (order.status.trim() != status.trim()){
+            statusMatch = false;
+          }
+        }
+        
+        if (searchMatch && statusMatch && cardMatch){
+          this.displayOrder.push(order);
+        }
+      });
+
+      this.length = this.displayOrder.length;
+      this.updateRange();
+    }
+
+    this.initializing = false;
+    this.withRecords = this.length > 0;
   }
 }
