8fb10fe3cb19834b2bd024916dbb1ff5d54aa9fa
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2022-09-23 21:30:54 +0800

    Card Upgrade
    Include Card Type on Order List

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -2,20 +2,20 @@ import { firestore } from "firebase";
 import Timestamp = firestore.Timestamp
 
 export class Order {
-    public id? : string;
+    public id?: string;
     public user_id?: string;
-    public card_id? : string;
-    public card_price? : number;
-    public sender_name? : string;
-    public sender_phone? : string;
-    public sender_email? : string;
-    public receiver_name? : string;
-    public receiver_phone? : string;
-    public receiver_email? : string;
+    public card_id?: string;
+    public card_price?: number;
+    public sender_name?: string;
+    public sender_phone?: string;
+    public sender_email?: string;
+    public receiver_name?: string;
+    public receiver_phone?: string;
+    public receiver_email?: string;
     public address?: string;
-    public anonymously? : boolean;
-    public sendto? : string;
-    public message? : string;
+    public anonymously?: boolean;
+    public sendto?: string;
+    public message?: string;
     public created?: Timestamp;
     public isPaid?: Boolean;
     public paymentId?: string;
@@ -23,4 +23,5 @@ export class Order {
     public parentOrder?: string;
     public count?: number;
     public shipping_fee?: number;
+    public type?: string;
 }
